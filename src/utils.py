80239d50628dc1e6f486c31571c787dcb7c75adf
Russell Kim <russellkim98@gmail.com>
2025-09-20 12:27:36 +0900

    fresh start

--- src/utils.py
+++
@@ -1,8 +0,0 @@
-"""Utility functions for the application."""
-
-def format_duration(ms: int) -> str:
-    """Convert milliseconds to MM:SS format."""
-    seconds = ms // 1000
-    minutes = seconds // 60
-    seconds = seconds % 60
-    return f"{minutes}:{seconds:02d}"
